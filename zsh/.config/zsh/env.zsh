3d6fd10f7b46d734885147c90f05ee15ec5d752b
captain <cap15369@gmail.com>
2025-12-08 16:19:59 +0800

    代理7897端口可以时默认连接代理

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -44,9 +44,48 @@ export EDITOR=nvim
 # windows下设置环境变量
 # set OLLAMA_HOST=0.0.0.0
 
+# 在tmux中启用fzf-tab弹窗
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
 # 使用 shell 提示的 Yazi 用户可能希望显示一个指示器，以便轻松知道他们位于 yazi 子 shell 内。
 YAZI_TERM=""
 if [ -n "$YAZI_LEVEL" ]; then
 	YAZI_TERM=" : "
 fi
 PS1="$PS1$YAZI_TERM"
+
+# 检测并设置代理
+zmodload zsh/net/tcp
+if ztcp 127.0.0.1 7897 2>/dev/null; then
+    local fd=$REPLY
+    ztcp -c $fd
+    export ALL_PROXY="http://127.0.0.1:7897"
+    export http_proxy="http://127.0.0.1:7897"
+    export https_proxy="http://127.0.0.1:7897"
+    # 设置一个变量供 Prompt 使用
+    export PROXY_STATE="🌐" 
+else
+    export PROXY_STATE=""
+fi
+
+# 将图标添加到右侧提示符
+# RPROMPT="$RPROMPT $PROXY_STATE"
+
+# 连接tmux或新建会话
+# 检测 tmux 是否安装
+if command -v tmux &> /dev/null; then
+	# 1. 检测是否是交互式 Shell (防止 scp/sftp 等非交互连接被破坏)
+	# 2. 检测当前是否已经在 tmux 里面 (防止无限递归)
+	# 3. 检测是否在 VSCode 的内置终端里
+	# 4. 检测是否在 Neovim 内置终端里
+	if [[ $- == *i* ]] && \
+		[ -z "$TMUX" ] && \
+		[ "$TERM_PROGRAM" != "vscode" ] && \
+		[ -z "$NVIM" ]; then
+		# 尝试进入 main 会话
+		# 使用 exec 可以替换当前 Shell 进程，这样退出 tmux 时会直接关闭终端，而不是退回到外层 Shell
+		exec tmux new-session -A -s main
+	fi
+fi
+
+  
